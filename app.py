7feb7c663ad7c45c8ef72e70bf4dfac0c64fcb6d
lofiwanderer <lofiwanderer@gmail.com>
2025-06-18 22:25:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -767,12 +767,12 @@ def analyze_data(data, pink_threshold, window_size):
         
     if len(df["score"].fillna(0).values) > 20:
         if upper_slope is not None:
-            upper_slope = safe_round(latest.get('upper_slope')) if 'upper_slope' in latest else 0), )
-            lower_slope = safe_round(latest.get('lower_slope')) if 'lower_slope' in latest else 0), )
-            upper_accel = safe_round(latest.get('upper_accel')) if 'upper_accel' in latest else 0), )
-            lower_accel = safe_round(latest.get('lower_accel')) if 'lower_accel' in latest else 0), )
-            bandwidth = safe_round(latest.get('bandwidth')) if 'bandwidth' in latest else 0), )
-            bandwidth_delta = safe_round(latest.get('bandwidth_delta')) * 100 if 'bandwidth_delta' in latest else 0), )
+            upper_slope = safe_round(latest.get('upper_slope')) if 'upper_slope' in latest else 0 
+            lower_slope = safe_round(latest.get('lower_slope')) if 'lower_slope' in latest else 0
+            upper_accel = safe_round(latest.get('upper_accel')) if 'upper_accel' in latest else 0
+            lower_accel = safe_round(latest.get('lower_accel')) if 'lower_accel' in latest else 0
+            bandwidth = safe_round(latest.get('bandwidth')) if 'bandwidth' in latest else 0
+            bandwidth_delta = safe_round(latest.get('bandwidth_delta')) * 100 if 'bandwidth_delta' in latest else 0
                 
         
     
